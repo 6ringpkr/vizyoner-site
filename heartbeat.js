7d2acbe9acdbcf7e258f63789fe616446a903332
val <vallico.visionaire@gmail.com>
2025-07-31 09:35:55 +0800

    working node server

--- heartbeat.js
+++ heartbeat.js
@@ -27,10 +27,24 @@ app.use(bodyParser.json());
 app.use(express.static('.')); // Serve static files from current directory
 
 // Enable CORS for development
+// Enhanced CORS middleware
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  const allowedOrigins = [
+    'http://127.0.0.1:8086',
+    'http://localhost:8086',
+    'http://127.0.0.1:8085',
+    'http://localhost:8085'
+  ];
+  
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
   if (req.method === 'OPTIONS') {
     res.sendStatus(200);
   } else {
